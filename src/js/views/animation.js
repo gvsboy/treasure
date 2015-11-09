ab91d4daf1bfdcfc8e3309a5a32bae5cfb76d29d
Kevin Mikles <kmikles@linkedin.com>
2015-11-08 21:08:49 -0800

    Hacking away at some animation

--- src/js/views/animation.js
+++ src/js/views/animation.js
@@ -3,41 +3,99 @@ import _ from 'lodash';
 import Velocity from 'velocity-animate';
 import cardView from './card';
 
-function animate(id, card) {
+// Maybe this should be moved to the VM ...
+function animate(opts, card) {
 
   // Get a reference to the real card.
-  var realCard = document.getElementById(id);
+  var realCard = document.getElementById(opts.card.id),
+      board = document.getElementById('board'),
+      flipper = card.querySelector('.flipper');
 
   // First, let's position the animated card over the real card.
   card.style.top = realCard.offsetTop + 'px';
   card.style.left = realCard.offsetLeft + 'px';
 
+  // Set the board opacity a bit lower to highlight the animated cards.
+  board.style.opacity = 0.2;
+
+  // Configure the placement and grow properties. Different lefts for each card.
   var properties = {
-    top: '20px',
-    left: '100px'
+    top: (board.offsetTop + ((board.offsetHeight / 2) - (realCard.offsetHeight))) + 'px',
+    width: '6em',
+    height: '6em'
   };
+  if (opts.index === 0) {
+    properties.left = (board.offsetLeft + (realCard.offsetWidth / 2)) + 'px';
+  }
+  else {
+    properties.left = (board.offsetLeft + (realCard.offsetWidth * 4.75)) + 'px';
+  }
 
-  var options = {
-    duration: 2000
-  };
+  function revealTreasure() {
 
-  Velocity(card, properties, options);
+    // Determine which outcome will happen while the background color
+    // is solid white.
+
+    // Now change it back and continue with the collection process.
+    Velocity(flipper, {
+      backgroundColor: '#123456',
+      fill: '#ccc'
+    }, {
+      duration: 200
+    });
+
+  }
+
+  // Put the cards in their places.
+  Velocity(card, properties, {
+    duration: 1000
+  });
+
+  // All the while, spin and spin.
+  Velocity(card, {
+    rotateY: '1440deg'
+  }, {
+    duration: 3000,
+    easing: 'ease-out',
+    queue: false
+  });
+
+  // And after a delay, smash them together
+  Velocity(card, {
+    left: (board.offsetLeft + (realCard.offsetWidth * 2.6)) + 'px'
+  }, {
+    duration: 600,
+    delay: 2600,
+    queue: false,
+    complete: function() {
+      Velocity(flipper, {
+        backgroundColor: '#fff'
+      }, {
+        duration: 200,
+        complete: revealTreasure
+      });
+    }
+  });
+
 }
 
-var card = {
+var cardComponent = {
   view: cardView
 };
 
 export default function(ctrl, args) {
+
+  // Collect all the matched cards. There should be two.
   var cards = _.filter(ctrl.cards, card => ctrl.cardsVM(card.id).isMatched());
+
   return m('div.animations', [
-    _.map(cards, function(c) {
-      return m.component(card, {
-        id: 'animate-' + c.id,
+    _.map(cards, (card, index) => {
+      return m.component(cardComponent, {
+        id: 'animate-' + card.id,
         class: 'matched',
-        front: c.icon(),
-        back: c.icon(),
-        config: _.partial(animate, c.id)
+        front: card.icon(),
+        back: card.icon(),
+        config: _.partial(animate, { card, index })
       })
     })
   ]);
